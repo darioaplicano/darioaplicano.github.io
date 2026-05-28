5896ceeb2f58f108edfc1603a952c06d2afd5442
darioaplicano <alexdario.flores@telefonica.com>
2026-05-28 18:47:29 +0000

    Deploying to gh-pages from @ darioaplicano/darioaplicano.github.io@7515b34b9e556f6f3a8bd3369f830872c58d7f23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-publicaciones",
-          title: "publicaciones",
-          description: "Publicaciones academicas, ordenadas por año en orden descendente.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
         },{id: "nav-repositorios",
           title: "repositorios",
           description: "Perfil de GitHub y repos públicos. Código, ejercicios de cursos y proyectos personales en Python, Scala, TypeScript y más.",
